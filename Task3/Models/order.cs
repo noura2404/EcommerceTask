491f8640b4fb0bf52d37921edc0ed843067472ae
noura2404 <nouranour1617@gmail.com>
2018-11-17 12:40:56 +0200

    task 3 update

---
+++ Task3/Models/order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task3.Models
+{
+    public class Order
+    {
+
+        public int O_ID { get; set; }
+        public int C_ID { get; set; }
+        public String P_Name { get; set; }
+        public int Quantity { get; set; }
+        public string CAddress { get; set; }
+
+
+    }
+}
---
+++ Task3/Models/product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task3.Models
+{
+    public class product
+    {
+
+        public string P_Name { get; set; }
+        public int Quantity { get; set; }
+        public String Model { get; set; }
+    }
+}
